d471458d5db4fcf8ffe1aedb94d9460f70b91f8d
Ariav Buchris <ariav.buchris@gmail.com>
2020-10-28 10:50:21 +0200

    spring boot starter

---
+++ src/main/java/com/ariavbar/redditoperationsgw/RedditOperationsGwApplication.java
@@ -0,0 +1,13 @@
+package com.ariavbar.redditoperationsgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditOperationsGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditOperationsGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ariavbar/redditoperationsgw/RedditOperationsGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariavbar.redditoperationsgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditOperationsGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
